d829b3e4ad2c3408a6c2b2270b34fca0dc10f54c
Ryhan Chowdhury <ryhan.chowdhury3@gmail.com>
2018-11-09 14:15:04 -0600

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://elastic-mahavira-de2f8b.netlify.com/', /http:\/\/localhost:*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://elastic-mahavira-de2f8b.netlify.com', /http:\/\/localhost:*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
