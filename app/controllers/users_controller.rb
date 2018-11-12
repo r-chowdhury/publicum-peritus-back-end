bc91d22fb4d8096d676531584d15bed9ca511a7a
Ryhan Chowdhury <ryhan.chowdhury3@gmail.com>
2018-11-12 15:12:48 -0600

    don't create for invalid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,19 @@ class UsersController < ApplicationController
   before_action :user_params
   skip_before_action :authorized, only: [:create]
 
-  def profile
-    render json: { user: UserSerializer.new(current_user) }, status: :accepted
-  end
+  # def profile
+  #   render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  # end
 
   def create
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
+      GoogleCivicsApi.get_officials(@user)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
-    GoogleCivicsApi.get_officials(@user)
   end
 
   def index 
